ab0ee2dda7c56ad540f5a66827ba338d08d42e23
mjvini <40394180+mjvini@users.noreply.github.com>
2023-07-23 18:20:17 -0300

    Update CodeGenerator.py

--- codigos_finais/CodeGenerator.py
+++ codigos_finais/CodeGenerator.py
@@ -2,9 +2,25 @@
     Nomes: Gabriel Almeida Mendes - DRE: 117204959
        Marcus Vinicius Torres de Oliveira - DRE: 118142223
 """
+# Imports da enumeração para representar os opcodes
 from enum import Enum
 import json
 
+###########################################################
+###########################################################
+#
+#                   CODEGENETAOR
+#
+# Essa classe é responsável por gerar as instruções de
+# código intermediário a partir da arvore.
+#
+# OBS: CÓDIGO QUEBRADO
+###########################################################
+###########################################################
+"""
+Definindo a enumeração OpCode que representa as 
+diferentes operações suportadas
+"""
 class OpCode(Enum):
     EXIT = 0
     NUMBER = 1
@@ -29,7 +45,10 @@ class OpCode(Enum):
     JUMP = 20
     JUMP_TRUE = 21
     JUMP_FALSE = 22
-
+"""
+Classe do Codegenetaor responsável por gerar 
+instruções de código intermediário.
+"""
 class CodeGenerator:
     def __init__(self):
         self.instructions = []
@@ -37,10 +56,12 @@ class CodeGenerator:
     def generate(self, node):
         if node is None:
             return
-
+         # Obtendo o tipo do nó para decidir como gerar as instruções
         node_type = node.__class__.__name__
+        #Nums
         if node_type == 'NumeroDeNos':
             self.instructions.append({'op': OpCode.NUMBER.value, 'value': int(node.tok.valor)})
+        #Bin
         elif node_type == 'BinOpNo':
             self.generate(node.no_esq)
             self.generate(node.no_dir)
@@ -66,17 +87,19 @@ class CodeGenerator:
                 self.instructions.append({'op': OpCode.GE.value})
             elif node.op_tok.tipo == 'MAIORIGUAL':
                 self.instructions.append({'op': OpCode.GEQ.value})
+        #Unarios
         elif node_type == 'UnaryOpNo':
             self.generate(node.no)
             if node.op_tok.tipo == 'MENOS_UNARIO':
                 self.instructions.append({'op': OpCode.NEG.value})
             elif node.op_tok.tipo == 'NOT':
                 self.instructions.append({'op': OpCode.NOT.value})
-        # Adicione aqui outras classes de nós conforme necessário
-
+        
+    # Obtendo as instruções geradas
     def get_instructions(self):
         return self.instructions
-
+# Função para serializar as instruções
+# geradas em formato JSON e gravá-las em um arquivo
 def serialize_instructions(instructions, output_file):
     with open(output_file, 'w') as file:
         json.dump(instructions, file)
